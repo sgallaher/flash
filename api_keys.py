acde9ef9d9cc1f1de02eca226c1a0194fa946180
sgallaher <54886009+sgallaher@users.noreply.github.com>
2024-12-17 14:52:35 +0000

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,3 @@
+CLIENT_ID = '248151550108-1hu92klpgrp0t3kuoanqgaivush6t8n4.apps.googleusercontent.com'
+CLIENT_SECRET='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'
+CODESPACE_NAME='bookish-couscous-9wgw756qw5hx944'
